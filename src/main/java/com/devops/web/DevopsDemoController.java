54d5155dd6ceab7e66534ee5138df92141efdf4e
xianyanYang <sakura521yz@gmail.com>
2018-06-05 18:34:08 +0800

    修改源码，测试Jenkins定时轮询服务

--- src/main/java/com/devops/web/DevopsDemoController.java
+++ src/main/java/com/devops/web/DevopsDemoController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * DevopsDemoController资源
+ */
 @RestController
 public class DevopsDemoController {
 
+    /**
+     * hello world
+     *
+     * @param name 姓名
+     * @return 打招呼
+     */
     @GetMapping(value = "/sayHi/{name}")
     public String sayHi(@PathVariable(name = "name") String name) {
-        return String.format("hello! %s !", name);
+        return String.format("hello World ! %s !", name);
     }
 }
